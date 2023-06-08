b6729b73ad1b4089cc88026e2adb578e0af632d6
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-06-08 18:14:16 +0800

    add comment

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,34 +1,47 @@
 import React, {useState, useEffect} from 'react'
 import {User} from '../types/User'
-import Refresh from './Refresh'
+import http from '../api/http'
 
-const UserInfo: React.FC<object>= () => {
-    const [user, setUser] = useState<User[]>([])
 
-    useEffect(() => {
-        const fetchUser = () => {
-            const userData = localStorage.getItem('user')
-            if(userData){
-                const parsedData: User[] = JSON.parse(userData);
-                setUser(parsedData)
-            }
+const UserInfo: React.FC = () => {
+    const [user, setUser] = useState<User | null>(null)
+
+    // Fetch the User from the API and store the data to local storage
+    const fetchUser = async () => {
+        try {
+            const response = await http.get('/');
+            const userData = await response.data.results[0];
+            localStorage.setItem('user', JSON.stringify(userData))
+            setUser(userData)
+        } catch(err) {
+            console.log('Error fetching the user', err)
         }
-        fetchUser();
+    }
+    // Display the User Info
+    useEffect(() => {
+        const savedUser = localStorage.getItem('user')
+        if(savedUser){
+            const userInfo = JSON.parse(savedUser)
+            console.log(userInfo)
+            setUser(userInfo)
+        } else {
+            fetchUser();
+        }
     },[])
 
-    if(!user){
-        return null
+    // Refresh the Component and Change the User Display
+    const handleRefresh = () => {
+        fetchUser();
     }
+
   return (
     <div className="d-flex justify-content-center flex-column align-items-center mx-auto vh-100 text-center">
-        {user.map((u, index) => (
-            <div key={index}>
-                <img src={u.picture.medium} className="rounded-circle img-thumbnail"/>
-                <h3>{u.name.first} {u.name.last}</h3>
-                <p>{u.email}</p>
-            </div>
-        ))}
-        <Refresh/>
+        {!user ? <p>Loading...</p> : <div>
+                <img src={user.picture.medium} alt={user.name.first} className="rounded-circle thumbnail my-2"/>
+                <h3>{user.name.first} {user.name.last}</h3>
+                <p>{user.email}</p>
+            </div>}
+        <button className="btn btn-primary" onClick={handleRefresh}>Refresh</button>
     </div>
   )
 }
